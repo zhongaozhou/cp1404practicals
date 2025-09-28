e99588e6b6bff48a5d5aafadda52d297fcbb3c1d
Aozhou Zhong <aozhou.zhong@my.jcu.edu.au>
2025-09-28 11:07:27 +0800

    refactor score, temperatures from prac 1

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,14 +3,30 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-elif score <= 100:
-    print("Excellent")
-else:
-    print("Invalid score")
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score_status(score)
+    print(result)
+
+    # Random score check
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score}")
+    print(determine_score_status(random_score))
+
+
+def determine_score_status(score):
+    """Determine the result of a score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -6,19 +6,35 @@ Program for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("fahrenheit: "))
-        celsius = 5. / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return 5.0 / 9 * (fahrenheit - 32)
+
+
+main()
