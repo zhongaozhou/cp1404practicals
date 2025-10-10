e9515c3e9239f28a06d3dff8710e082397c4bbd3
Aozhou Zhong <aozhou.zhong@my.jcu.edu.au>
2025-10-10 15:59:22 +0800

    update subject reader

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,28 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    subject_records = load_subject_data(FILENAME)
+    display_details(subject_records)
 
 
-def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
+def load_subject_data(filename=FILENAME):
+    """Read data from file formatted like: subject,lecturer,number of students and return a list."""
+    subject_records = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_records.append(parts)  # Add the record to list of subjects
     input_file.close()
+    return subject_records
+
+
+def display_details(subject_records):
+    """Display the subject details including name, lecturer and number of students."""
+    for record in subject_records:
+        subject, lecturer, num_students = record
+        print(f"{subject} is taught by {lecturer} and has {num_students} students")
 
 
 main()
