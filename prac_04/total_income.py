4d229bf5e9b65864f7942bb26010eb79d13b0dea
Aozhou Zhong <aozhou.zhong@my.jcu.edu.au>
2025-10-10 15:36:02 +0800

    finish total income

---
+++ prac_04/total_income.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 Practical
+Cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+    print_income_report(incomes, number_of_months)
+
+
+def print_income_report(incomes, number_of_months):
+    """Print income report showing income and cumulative total for each month."""
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, number_of_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+
+main()
