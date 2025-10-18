240d3fa7ca519f78fb3d4751040a20f3c2dea090
Aozhou Zhong <aozhou.zhong@my.jcu.edu.au>
2025-10-18 16:37:31 +0800

    complete Word Occurrences in Prac 5

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 Practical
+Word Occurrences
+Estimate: 20 minutes
+Actual:   30 minutes
+"""
+
+# Ask the user for input text
+text = input("Text: ")
+
+# Split the text into words
+words = text.split()
+
+# Create a dictionary to count word occurrences
+word_count = {}
+for word in words:
+    word = word.lower()
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+# Find the length of the longest word
+max_length = 0
+for word in word_count:
+    if len(word) > max_length:
+        max_length = len(word)
+
+
+# Sort the dictionary by key and print the results
+for word in sorted(word_count):
+    print(f"{word:{max_length}} : {word_count[word]}")
