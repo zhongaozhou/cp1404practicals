a5b0419fbea5f5859c90dca7ceaa1e662d61bd16
Aozhou Zhong <aozhou.zhong@my.jcu.edu.au>
2025-11-08 07:54:34 +0800

    init guitar and csv file

---
+++ prac_07/guitar.py
@@ -0,0 +1,32 @@
+"""CP1404/CP5632 Practical - Guitar class.
+Estimated time: 35 minutes
+Actual time: 40 minites
+"""
+
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance.
+
+        name: string, name of the guitar
+        year: integer, year the guitar was made
+        cost: float, cost of the guitar
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string of the Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return how old the guitar is in years."""
+        current_year = 2025
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old, False if not."""
+        return self.get_age() >= 50
