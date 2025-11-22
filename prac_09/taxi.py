b303086b2feb7d9994915682bb6335a0f67b9a78
Aozhou Zhong <aozhou.zhong@my.jcu.edu.au>
2025-11-23 07:38:49 +0800

    add round to 10 cents for get_fare

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -20,7 +20,8 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        fare = self.price_per_km * self.current_fare_distance
+        return round(fare, 1)
 
     def start_fare(self):
         """Begin a new fare."""
